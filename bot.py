0efd29d0d46f3179c74a1668f73a0e42a5560100
Christopher Ptak <cptak@comcast.net>
2021-02-19 22:09:47 -0500

    Added basic dice mocking

--- bot.py
+++ bot.py
@@ -1,62 +1,28 @@
 #!/bin/env python3
 
-# Standard library imports
 import os
-import sqlite3
-
-# Import discord API
 import discord
 
 
-##
-## A client class for this specific discord bot.
-##
-## Extends discord.Client and overrides event handlers.
-## This template also creates a data directory and a database connection for
-## bot-specific data. For example, a bot may need to remember which users it
-## has interacted with, or keep settings for how to behave on each server.
-##
+class DiceMockerClient(discord.Client):
 
-class DiscordBotClient(discord.Client):
-
-    def __init__(self):
-
-        # Call discord.Client constructor
-        super().__init__()
-
-        # Create data directory if it does not exist
-        if not os.path.isdir('data'):
-            os.mkdir('data')
-
-        # Create or open a database for bot data
-        self.database = sqlite3.connect('data/bot.db')
-        self.dbcursor = self.database.cursor()
-
-    def __del__(self):
-        # Save changes and close the database
-        self.database.commit()
-        self.database.close()
-        print('Stopped DiscordBotClient')
-
-
-    ##
-    ## Example event handlers
-    ##
-
-    # Event handler for bot startup
     async def on_ready(self):
-        print('Started DiscordBotClient')
+        print('Started DiceMockerClient')
 
-    # Event handler for a text-channel message
     async def on_message(self, message):
-        print('Saw message: {}'.format(message.content))
+
+        if message.author.name != 'DiceParser':
+            return
+
+        lines = message.content.split('\n')
+        roll_value = int(lines[1].split()[1])
+
+        if roll_value <= 2:
+            await message.channel.send('lmao loser')
 
 
-
-# Read the token from a file
 token = open('token.txt', 'r').read()
 
-# Run the bot
-client = DiscordBotClient()
+client = DiceMockerClient()
 client.run(token)
 
