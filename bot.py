4093bad5096c2f4320db9040e0959caaeceb0bd2
Christopher Ptak <cptak@comcast.net>
2021-02-22 21:08:36 -0500

    Implemented limited min/max calculation for DiceParser

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 
 import os
 import random
+import re
 
 import discord
 
@@ -17,47 +18,105 @@ class DiceMockerClient(discord.Client):
     async def on_ready(self):
         print('Started DiceMockerClient')
 
+    ##
+    ## React to a message send in a public discord channel.
+    ## Check to see if the message was sent by one of the supported dice bots,
+    ## and if so parse the message and respond appropriately.
+    ##
     async def on_message(self, message):
 
         if message.author.name == 'DiceParser':
-            roll = self._parse_DiceParser(message.content)
+            (roll, minval, maxval) = self._parse_DiceParser(message.content)
         elif message.author.name == 'Avrae.avrae':
-            roll = self._parse_Avrae(message.content)
+            (roll, minval, maxval) = self._parse_Avrae(message.content)
         else:
             # Ignore all messages not from a Dice bot
             return
 
-        if roll != RollStatus.IGNORE_ROLL:
-            await self._mock_reply(roll, message.channel.send)
+        status = self._judge_roll(roll, minval, maxval)
+        if status != RollStatus.IGNORE_ROLL:
+            await self._mock_reply(status, message.channel.send)
+
+    ## The below functions are the individual parsers for the outputs of the
+    ## various dice bots. All return a 3-tuple of the actual value of the
+    ## roll, the minimum value that could have been rolled, and the maximum
+    ## possible value.
 
     def _parse_DiceParser(self, content):
 
-        value_line = content.split('\n')[1]
-        final_value = int(value_line.split()[1])
+        roll = None
+        minval = None
+        maxval = None
 
-        # TODO: Implement a calculation of these quantities
-        min_possible_value = 0
-        max_possible_value = 1
+        for line in content.split('\n'):
+            tokens = line.split()
+            if len(tokens) == 2 and tokens[0] == '#':
+                roll = int(tokens[1])
+            elif len(tokens) > 0 and tokens[0].startswith('Details'):
 
-        if max_possible_value == min_possible_value:
-            # Ignore trivial rolls like a d0 or d1
+                expression = tokens[0][9:]
+                minval = 0
+                maxval = 0
+
+                # TODO: Come up with a better way of estimating min and max
+
+                for token in re.split('[+-]', expression):
+
+                    match = re.fullmatch('[0-9]+', token)
+                    if match is not None:
+                        minval += int(match.group(0))
+                        maxval += int(match.group(0))
+                        continue
+
+                    match = re.fullmatch('d([0-9]+)', token)
+                    if match is not None:
+                        minval += 1
+                        maxval += int(match.group(1))
+                        continue
+
+                    match = re.fullmatch('([0-9]+)d([0-9]+)', token)
+                    if match is not None:
+                        minval += int(match.group(1))
+                        maxval += int(match.group(1)) * int(match.group(2))
+                        continue
+
+                    raise ValueError('Unrecognized token \'' + token
+                                     + '\' in DiceParser output')
+
+        if None in [roll, minval, maxval]:
+            raise ValueError('Unable to find all parts of DiceParser output')
+
+        return (roll, minval, maxval)
+
+    def _parse_Avrae(self, content):
+        raise NotImplementedError
+
+    ##
+    ## Categorize a roll based on the three parameters from the parsers.
+    ## Returns either IGNORE_ROLL, LOW_ROLL, or CRIT_FAIL.
+    ##
+    def _judge_roll(self, roll, minval, maxval):
+
+        if (maxval - minval) < 3:
+            # Ignore trivial rolls
             return RollStatus.IGNORE_ROLL
 
-        if final_value == min_possible_value:
+        if roll == minval:
             return RollStatus.CRIT_FAIL
-        if final_value <= max_possible_value * 0.25:
+        if roll < maxval * 0.25:
             # Anything below 25% of the max is considered a low roll
             return RollStatus.LOW_ROLL
         else:
             # All other rolls should be ignored
             return RollStatus.IGNORE_ROLL
 
-    def _parse_Avrae(self, content):
-        raise NotImplementedError
+    ##
+    ## Given a category of reply and a callback function to send a message,
+    ## choose an appropriate taunt and send it using the callback function.
+    ##
+    async def _mock_reply(self, status, send):
 
-    async def _mock_reply(self, roll, send):
-
-        if roll == RollStatus.CRIT_FAIL and random.random() < 0.25:
+        if status == RollStatus.CRIT_FAIL and random.random() < 0.25:
 
             # Have a 25% chance to send a tutorial link on a crit fail
             embed = discord.Embed(url='https://dnd.wizards.com/get-started',
@@ -70,7 +129,7 @@ class DiceMockerClient(discord.Client):
 
             await send('Maybe this will help you', embed=embed)
 
-        elif roll == RollStatus.LOW_ROLL:
+        else:
 
             # Create a random taunt from these choices
             laugh = random.choice(['haha', 'lol', 'lmao', '😂'])
